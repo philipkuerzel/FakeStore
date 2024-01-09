fae288aced16e5fb153be96c9b88e20260fb75c3
philipkuerzel <philipkuerzel@gmail.com>
2024-01-09 16:41:09 +0100

    update html and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,14 @@ fetch('https://fakestoreapi.com/products')
     })
     .catch((err) => console.log(err));
 
-let productContainer = document.querySelector(".product-container");
+let productContainer = document.querySelector(".product-container");    //section for the products
 let products;
 
-const displayProducts = (products) => {
-    productContainer.innerHTML = "";
+const displayProducts = (products) => {     // function to render and show the products
+    productContainer.innerHTML = "";        
 
     products.forEach((product) => {
-        const productTitle = product.title;
+        const productTitle = product.title;     //set the elements from api 
         const productImg = product.image;
         const productPrice = product.price;
 
@@ -41,24 +41,24 @@ const displayProducts = (products) => {
     });
 };
 
-const sortProducts = (sortType) => {
+const sortProducts = (sortType) => {        // function to sort the products by price (ascending or descending)
     if (sortType === "priceUp") {
         products.sort((a, b) => a.price - b.price);
     } else if (sortType === "priceDown") {
         products.sort((a, b) => b.price - a.price);
     }
 
-    displayProducts(products);
+    displayProducts(products); // show sorted products
 };
 
-const filterProducts = (category) => {
-    const filteredProducts = category === 'all' ? products : products.filter(
+const filterProducts = (category) => {      //function to filter products by category (electronics, jewelery, men's clothing or women's clothing)
+    const filteredProducts = category === 'all' ? products : products.filter(   // function is called by clicking on the button
         (product) => product.category.toLowerCase().includes(category)
     );
-    displayProducts(filteredProducts);
+    displayProducts(filteredProducts);  // show filtered products
 };
 
-const searchProducts = () => {
+const searchProducts = () => {      // function to search products by given text
     const searchInput = document.getElementById("search").value.toLowerCase();
     const searchedProducts = products.filter((product) =>
         product.title.toLowerCase().includes(searchInput)
@@ -66,12 +66,12 @@ const searchProducts = () => {
     displayProducts(searchedProducts);
 };
 
-document.getElementById("sort").addEventListener("change", (event) => {
+document.getElementById("sort").addEventListener("change", (event) => {     // function sortProducts is called bei changing the select element
     const selectedSort = event.target.value;
     sortProducts(selectedSort);
 });
 
-document.getElementById("search").addEventListener("keyup", (event) => {
+document.getElementById("search").addEventListener("keyup", (event) => {    //function searchProducts is called by typing text into the input field
     event.preventDefault();
     searchProducts();
 });
